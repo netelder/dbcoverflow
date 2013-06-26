33ea4aff7e602c172d883d5b2fbe51aaabf4b8c5
Apprentice <apprentice@dbc04.local>
2013-06-26 11:19:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbcOverflow::Application.config.secret_token = '666959aedc790ee8be18374f80d98e43912378386c1bb50daceb0af89cf0a5407949f23d0cb26bb32eda3eda02b23830b007c5fa86f24f0bd755c65a0a5eab39'
---
+++ db/migrate/20130626181614_users.rb
@@ -0,0 +1,10 @@
+class Users < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      
+    end
+  end
+
+  def down
+  end
+end
