baa36476f29fdc93032bfd5f1885ecf7c7d7915f
Apprentice <apprentice@dbc04.local>
2013-06-26 21:33:06 -0700

    Comment post complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,14 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new()
+    @post= Post.find(params[:post_id])
+
+  end
+
+  def create
+    comment = Comment.create(text: params[:comment][:text], post_id: params[:post_id], user_id: current_user.id)
+    redirect_to post_path(params[:post_id]), alert: "Thanks for your comment!"
+  end
+
 end
