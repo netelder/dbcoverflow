67c98315fabd7240f786819f89ce864e79649988
Lloyd Taylor <ltaylor@netelder.com>
2013-06-28 17:47:58 -0700

    added gravatar to user profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = Post.where(user_id: params[:id])
+    @gravatar = Digest::MD5.hexdigest(@user.email.downcase)
   end
 end
